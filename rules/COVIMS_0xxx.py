be35b0ecc50d1fc7baa4490f8230cf1efc6564f3
Robert French <rfrench@seti.org>
2018-09-27 17:23:23 -0700

    Fix VIMS opus_id again.

--- rules/COVIMS_0xxx.py
+++ rules/COVIMS_0xxx.py
@@ -151,8 +151,8 @@ opus_products = translator.TranslatorByRegex([
 filespec_to_opus_id = translator.TranslatorByRegex([
     # There are up to two OPUS IDs associated with each VIMS file, one for the VIS channel and one for the IR channel.
     # This translator returns the OPUS ID without the suffix "_IR" or "_VIS" used by OPUS. That must be handled separately
-    (r'COVIMS_00../(data|extras)/.*/(v[0-9]{10})_[0-9]+\..+$',  0, r'co.vims.\2'),
-    (r'COVIMS_00../(data|extras)/.*/(v[0-9]{10})_[0-9]+(_[0-9]{3})\..+$',  0, r'co.vims.\2\3'),
+    (r'COVIMS_00../(data|extras)/.*/(v[0-9]{10}_[0-9]+)\..+$',  0, r'co.vims.\2'),
+    (r'COVIMS_00../(data|extras)/.*/(v[0-9]{10}_[0-9]+_[0-9]{3})\..+$',  0, r'co.vims.\2'),
 ])
 
 ####################################################################################################################################
