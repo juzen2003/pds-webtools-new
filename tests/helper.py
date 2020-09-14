cd81ec88a0357c2436b363443d6e50e5b71ceda5
Yu-Jen Chang <juzen2003@gmail.com>
2020-09-14 15:29:16 -0700

    Update tests on opus products (#27)
    * Update opus_products blackbox tests for cocir_0xxx, covims_0xxx, hstix_xxxx,
    and coiss_2xxx.
    
    * Create compare_opus_products in helper.py to be re-used for opus products
    blackbox tests, and add one more test case for hstix_xxxx opus_products
    test.
    
    * Add & update tests for opus_products in coiss_1xxx, coiss_2xxx,
    hstix_xxxx, covims_0xxx, covims_8xxx, cocir_0xxx, couvis_0xxx,
    couvis_8xxx, and corss_8xxx. Rename comapre_opus_products to
    opus_products_test.
    
    * Add opus_products tests for ebrocc_xxxx, go_0xxx, NHxxxx_xxxx, and
    vgiss_5xxx.
    
    * Remove redundant test case of HSTIx_xxxx for opus_products test.
    
    * Update the test case for test_opus_products_go_0xxx by removing the v1
    index file.

--- tests/helper.py
+++ tests/helper.py
@@ -33,3 +33,23 @@ def get_pdsgroups(paths_group, is_abspath=True):
         pdsgroup = pdsfile.PdsGroup(pdsfiles=pdsfiles)
         pdsgroups_arr.append(pdsgroup)
     return pdsgroups_arr
+
+def opus_products_test(input_path, expected):
+    target_pdsfile = instantiate_target_pdsfile(input_path)
+    res = target_pdsfile.opus_products()
+    msg = (f'Total number of products ({len(res)}) does not match the expected'+
+           f' results ({len(expected)})')
+    assert len(res) == len(expected), msg
+    for key in res:
+        assert key in expected
+        all_files = []
+        all_files_abspath = []
+        for files in res[key]:
+            all_files += files
+        msg = f'Total number of files does not match under {key}'
+        assert len(all_files) == len(expected[key]), msg
+        for pdsf in all_files:
+            assert pdsf.abspath in expected[key]
+            all_files_abspath.append(pdsf.abspath)
+        msg = f'Files does not match under {key}'
+        assert all_files_abspath.sort() == expected[key].sort(), msg
