a2bc955b4b5c8b74da6dd7ecd6474582bf31daad
Mark Showalter <mshowalter@seti.org>
2022-03-16 18:23:26 -0700

    Remove validation tools that really belong to Viewmaster

--- validation/viewmaster_stress_test.sh
+++
@@ -1,9 +0,0 @@
-open http://mark.local/viewmaster/metadata
-open http://mark.local/viewmaster/calibrated/COISS_1xxx_v1.0/COISS_1003/data/1353850619_1353904680
-open http://mark.local/viewmaster/calibrated/COISS_1xxx/COISS_1003/data/1353756268_1353778349
-open http://mark.local/viewmaster/volumes/NHxxLO_xxxx/NHJULO_2001/data/20070108_003059
-open http://mark.local/viewmaster/archives-metadata/NHxxLO_xxxx/
-open http://mark.local/viewmaster/volumes/VGISS_7xxx/VGISS_7205/DATA/C26607XX?pages=8
-open http://mark.local/viewmaster/volumes/VGISS_7xxx/VGISS_7205/DATA/C26612XX?pages=8&grid=2
-open http://mark.local/viewmaster/diagrams/COCIRS_6xxx/COCIRS_6002/BROWSE/S_RINGS?pages=8&grid=1
-
